b59046677dcf002c3f455f1a7aff3e4562090711
manojsundar18 <manoj@pyramidions.in>
2023-09-28 16:21:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+// import Userlogin from "./component/UserLogin/Userlogin";
+import Login from "./component/Login/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      {/* <Userlogin /> */}
+    </div>
+  );
+}
+
+export default App;
